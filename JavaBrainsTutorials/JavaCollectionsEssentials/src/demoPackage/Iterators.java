25adc1f8c7452003cec5519248a59f0b1f64f7ca
ktadewos <kaleab.ero@mediaagility.com>
2021-12-28 10:36:38 -0600

    demo code for working on forEachRemaining

---
+++ JavaBrainsTutorials/JavaCollectionsEssentials/src/demoPackage/Iterators.java
@@ -0,0 +1,35 @@
+package demoPackage;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class Iterators {
+
+	public static void main(String[] args) {
+		
+		//foreachremaining method is used to to anything you want for the remaining element of the list on which your iterator is not iterated through.
+		//what is does is the same as its name saying.
+		
+		List<String> stringArray = Arrays.asList("a", "b", "c", "d");
+		
+		
+		Iterator<String> iterator = stringArray.iterator();
+		Iterator<String> iterator2 = stringArray.iterator();
+		iterator2.forEachRemaining((a) -> System.out.println(a + "here"));
+		int i = 0;
+		
+		while(iterator.hasNext()) {
+			i++;
+			System.out.println(iterator.next());
+			iterator.forEachRemaining((a) -> System.out.println(a + "here"));
+		}
+		
+		System.out.println(i);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
