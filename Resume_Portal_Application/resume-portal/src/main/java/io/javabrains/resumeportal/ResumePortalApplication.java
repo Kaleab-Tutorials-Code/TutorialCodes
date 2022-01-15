8dc04156481fd18e32633192fc25e5ca97fc7f50
kaleab tadewos ero <tadewoskaleab@gmail.com>
2022-01-14 18:24:47 -0600

    copy codes from java brains

--- Resume_Portal_Application/resume-portal/src/main/java/io/javabrains/resumeportal/ResumePortalApplication.java
+++ Resume_Portal_Application/resume-portal/src/main/java/io/javabrains/resumeportal/ResumePortalApplication.java
@@ -1,9 +1,11 @@
-package io.kaleab.resumeportal;
+package io.javabrains.resumeportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ResumePortalApplication {
 
 	public static void main(String[] args) {
--- Resume_Portal_Application/resume-portal/src/test/java/io/javabrains/resumeportal/ResumePortalApplicationTests.java
+++ Resume_Portal_Application/resume-portal/src/test/java/io/javabrains/resumeportal/ResumePortalApplicationTests.java
@@ -1,4 +1,4 @@
-package io.kaleab.resumeportal;
+package io.javabrains.resumeportal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
